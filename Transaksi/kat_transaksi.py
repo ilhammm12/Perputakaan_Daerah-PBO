a3ef719956ad87a8f7dcbf92430b53e4aea1d118
Muhammad ilham <muhilham0603@gmail.com>
2020-04-23 10:04:29 +0700

    Decorator peminjaman

--- Transaksi/kat_transaksi.py
+++ Transaksi/kat_transaksi.py
@@ -1,36 +1,46 @@
 class Peminjaman:
-    def __init__(self, id_pinjam, id_agt, id_ptg, id_buku, tanggalpinjam, tanggalkembali, status):
-        self.id_pinjam = id_pinjam
-        self.id_agt = id_agt
-        self.id_ptg = id_ptg
-        self.id_buku = id_buku
-        self.tanggalpinjam = tanggalpinjam
-        self.tanggalkembali = tanggalkembali
+    def __init__(self, *args, **kwargs):
+        self.__id_pinjam = kwargs.get('idpinjam','001')
+        self.__id_agt = kwargs.get('idagt','001')
+        self.__id_ptg = kwargs.get('idptg','001')
+        self.__id_buku = kwargs.get('idbuku','001')
+        self.__tanggalpinjam = kwargs.get('tanggalpinjam','20 January 2020')
+        self.__tanggalkembali = kwargs.get('tanggalkembali','30 January 2020')
 
-    def get_id_pinjam(self):
-        return self.id_pinjam
-    def set_id_pinjam(self, value):
-        self.id_pinjam = value
+    @property
+    def id_pinjam(self):
+        return self.__id_pinjam
+    @id_pinjam.setter
+    def id_pinjam(self, value):
+        self.__id_pinjam = value
 
-    def get_id_agt(self):
-        return self.id_agt
-    def set_id_agt(self, value):
-        self.id_agt = value
+    @property
+    def id_agt(self):
+        return self.__id_agt
+    @id_agt.setter
+    def id_agt(self, value):
+        self.__id_agt = value
 
-    def get_id_ptg(self):
-        return self.id_ptg
-    def set_id_ptg(self, value):
-        self.id_ptg = value
+    @property
+    def id_ptg(self):
+        return self.__id_ptg
+    @id_ptg.setter
+    def id_ptg(self, value):
+        self.__id_ptg = value
 
-    def get_id_buku(self):
-        return self.id_buku
-    def set_id_buku(self, value):
-        self.id_buku = value
+    @property
+    def id_buku(self):
+        return self.__id_buku
+    @id_buku.setter
+    def id_buku(self, value):
+        self.__id_buku = value
 
-    def get_status(self):
-        return self.status
-    def set_status(self, value):
-        self.status = value
+    @property
+    def status(self):
+        return self.__status
+    @status.setter
+    def status(self, value):
+        self.__status = value
 
 class pengembalian() :
 
