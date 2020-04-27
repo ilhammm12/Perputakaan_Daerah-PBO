c62e58dd47fdb137cf78fc579854773f361c9beb
Muhammad ilham <muhilham0603@gmail.com>
2020-04-27 11:13:38 +0700

    Update kat_transaksi.py

--- Transaksi/kat_transaksi.py
+++ Transaksi/kat_transaksi.py
@@ -1,3 +1,7 @@
+from sqlachemy import create_engine, Column, Integer, String, Date, Boolean, ForeignKey
+from sqlachemy.ext.declarative import declarative_base
+from sqlachemy.orm import sessionmaker, relationship
+
 class Peminjaman:
     def __init__(self, *args, **kwargs):
         self.__id_pinjam = kwargs.get('idpinjam','001')
@@ -61,3 +65,19 @@ class pengembalian() :
     @denda.setter
     def denda(self, denda):
         self.denda = denda
+
+class tb_transaksi(Base):
+    __tablename__ = "tb_transaksi"
+
+    id_pinjam = Column('id_pinjam', Integer, primary_key=True)
+    id_anggota = Column('id_anggota', Integer, Unique=True)
+    id_petugas = Column('id_petugas', Integer, Unique=True)
+    id_buku = Column('id_buku', Integer, Unique=True)
+    tgl_pinjam = Column('tgl_pinjam', Date)
+    tgl_kembali = Column('tgl_kembali', Date)
+    status = Column('status', Boolean)
+    denda = Column('denda', Integer)
+
+engine = create_engine('sqllite:///:memory:', echo=True)
+Base.metadata.create_all(bind=engine)
+Session = sessionmaker(bind=engine)
