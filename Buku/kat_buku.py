cbb98b7abc19bee18dbca76f47130ee063d43dac
Wahyu Achmad Shafardan <61194981+Whyuachmadshfrdn@users.noreply.github.com>
2020-04-27 13:03:42 +0800

    Update kat_buku.py

--- Buku/kat_buku.py
+++ Buku/kat_buku.py
@@ -86,3 +86,40 @@ class buku():
     @no_rak.setter
     def no_rak(self, nomor_rak):
         self.__nomor_rak = nomor_rak
+
+from sqlalchemy import *
+from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import Column, Date, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+
+engine = create_engine('sqlite:///:memori', echo=True)
+Base = declarative_base()
+
+class buku(base):
+    __tablename__ = "buku"
+
+    id_buku = colum('id_buku', integer, primary_key=True)
+    judul_buku = colum('judul_buku', string, unique=True)
+    pengarang = colum('pengarang', string)
+    penerbit = colum('penerbit', string)
+    tahun_terbit = colum('tahun_ternit', string)
+    stok_buku = colum('stok_buku', string)
+    nomor_rak = colum('nomor_rak', integer)
+
+    def __init__(self, id_buku, judul_buku, pengarang, penerbit, tahun_terbit, stok_buku, nomor_rak):
+
+        self.id_buku = id_buku
+        self.judul_buku = judul_buku
+        self.pengarang = pengarang
+        self.penerbit = penerbit
+        self.tahun_terbit = tahun_terbit
+        self.stok_buku = stok_buku
+        self.nomor_rak = nomor_rak
+
+Base.metadata.create_all(engine)
+
+
+
+
+
