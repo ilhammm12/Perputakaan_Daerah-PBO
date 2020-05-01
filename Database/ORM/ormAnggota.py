2ec9eab9ad2bcf05bf1623ed30468c382e7b42fa
Muhammad ilham <muhilham0603@gmail.com>
2020-05-01 20:33:16 +0700

    Create ormAnggota.py

---
+++ Database/ORM/ormAnggota.py
@@ -0,0 +1,15 @@
+from sqlachemy import create_engine, Column, Integer, String, Date, Boolean, ForeignKey
+from sqlachemy.ext.declarative import declarative_base
+from sqlachemy.orm import sessionmaker, relationship
+
+class ormAnggota(Base):
+    __tablename__ = "tb_anggota"
+
+    id_regis = Column('id_regis', Integer, primary_key=True)
+    NIK = Column('NIK', Integer)
+    Nama = Column('Nama', String)
+    Tempat_lahir = Column('Tempat_lahir', String)
+    Tanggal_lahir = Column('Tanggal_lahir', Date)
+    Alamat = Column('Alamat', String)
+    No_hp = Column('No_hp', Integer)
+    Jenis_kelamin = Column('Jenis_kelamin', String)
