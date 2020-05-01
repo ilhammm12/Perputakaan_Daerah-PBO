ff18fa9412e3881492cb8ef88571faf0b823bcb0
Wahyu Achmad Shafardan <61194981+Whyuachmadshfrdn@users.noreply.github.com>
2020-05-01 21:58:01 +0800

    Add files via upload

---
+++ Class/Buku.py
@@ -0,0 +1,66 @@
+class buku():
+
+    def __init__(self, *args, **kwargs):
+        self.__id_buku = kwargs.get('id_buku', '1234')
+        self.__judul_buku = kwargs.get('judul_buku', 'Haliday')
+        self.__pengarang = kwargs.get('pengarang', 'albert')
+        self.__penerbit = kwargs.get('penerbit', 'jogjastore')
+        self.__tahun_terbit = kwargs.get('tahun_ternit', '2005')
+        self.__stok_buku = kwargs.get('stok_buku', '50')
+        self.__nomor_rak = kwargs.get('nomor_rak', '12')
+
+    @property
+    def id_buku(self):
+        return self.__id_buku
+
+    @id_buku.setter
+    def id_buku(self, id_buku):
+        self.__id_buku = id_buku
+
+    @property
+    def judul_buku(self):
+        return self.__judul_buku
+
+    @judul_buku.setter
+    def judul_buku(self, judul_buku):
+        self.__judul_buku = judul_buku
+
+    @property
+    def pengarang(self):
+        return self.__pengarang
+
+    @pengarang.setter
+    def pengarang(self, pengarang):
+        self.__pengarang = pengarang
+
+    @property
+    def penerbit(self):
+        return self.__penerbit
+
+    @penerbit.setter
+    def penerbit(self, penerbit):
+        self.__penerbit = penerbit
+
+    @property
+    def tahun_terbit(self):
+        return self.__tahun_terbit
+
+    @tahun_terbit.setter
+    def tahun_penerbit(self, tahun_terbit):
+        self.__tahun_terbit = tahun_terbit
+
+    @property
+    def stok(self):
+        return self.__stok_buku
+
+    @stok.setter
+    def stok(self, stok_buku):
+        self.__stok_buku = stok_buku
+
+    @property
+    def no_rak(self):
+        return self.__nomor_rak
+
+    @no_rak.setter
+    def no_rak(self, nomor_rak):
+        self.__nomor_rak = nomor_rak
