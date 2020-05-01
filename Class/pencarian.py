afa3c3f5e3846849fbf342697af8dcc530b0a872
Nyauliana <61154986+Nyauliana@users.noreply.github.com>
2020-05-01 23:32:59 +0800

    Add files via upload

---
+++ Class/pencarian.py
@@ -0,0 +1,7 @@
+class pencarian():
+    def __init__(self, id_pen, id_ang, id_buku, katakun, tgl_pen):
+        self.id_pencarian = id_pen
+        self.id_anggota = id_ang
+        self.id_buku = id_buku
+        self.kata_kunci = katakun
+        self.tanggal_pencarian = tgl_pen
