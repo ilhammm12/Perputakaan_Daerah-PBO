c0e58e6749224591f95f413bac4e6245651187b9
Muhammad ilham <muhilham0603@gmail.com>
2020-05-10 13:18:36 +0700

    Update ormBuku.py

--- Database/ORM/ormBuku.py
+++ Database/ORM/ormBuku.py
@@ -1,8 +1,5 @@
-from sqlalchemy import *
-from sqlalchemy import create_engine, ForeignKey
-from sqlalchemy import Column, Date, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
+from sqlachemy import create_engine, Column, Integer, String, Date, Boolean, ForeignKey
+from Database.base import Base, sessionFactory
 
 engine = create_engine('sqlite:///:memori', echo=True)
 Base = declarative_base()
