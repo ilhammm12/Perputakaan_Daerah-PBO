b31bdd3394604bcba01313fb147a57dc572d9611
Muhammad ilham <muhilham0603@gmail.com>
2020-05-10 13:19:59 +0700

    Update registrasi_orm.py

--- Database/ORM/registrasi_orm.py
+++ Database/ORM/registrasi_orm.py
@@ -1,11 +1,5 @@
-from sqlalchemy import *
-from sqlalchemy import create_engine, ForeignKey
-from sqlalchemy import Column, Date, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, backref
-
-engine = create_engine('sqlite:///:memori', echo=True)
-Base = declarative_base()
+from sqlachemy import create_engine, Column, Integer, String, Date, Boolean, ForeignKey
+from Database.base import Base, sessionFactory
 
 class registrasi_orm(base):
     __tablename__ = "registrasi"
